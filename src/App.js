eb81553efb8719a4fa9963e55bda7268ee35664a
hasan <hasan.maximmo@gmail.com>
2020-11-10 09:43:05 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {component} from 'react' ;
 
-function App() {
+import './App.css';
+import {Button , Form ,FormGroup ,Label, Input}
+from 'reactstrap' ;
+import {FacebookLoginButton} from 'react-social-login-buttons' ;
+class App extends React.Component{
+   render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Form className="login-form">
+      <h1>
+<span className="sp" > my website</span>
+         </h1>
+         <h2> welcome</h2>
+         <FormGroup>
+           <Label> Email</Label>
+<Input type="email" placeholder="email"/>
+         </FormGroup>
+         <FormGroup>
+         <label>  password   </label>
+         <Input type="password" placeholder='password'/>
+         </FormGroup>
+         <Button className="text-center pt-3"> login</Button> 
+<div>
+  Or with facebook account
+</div>
+<FacebookLoginButton className= "fb"/>
+<div className="text-center"><a href="signe-up"> signe up</a>   
+<span className="si"></span>
+<a href="signe-up"> forget  password </a>
+</div> 
+    </Form>
   );
 }
-
+ }
 export default App;
